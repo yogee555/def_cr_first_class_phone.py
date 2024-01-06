b3f94e25b150b7b6cb72a08c078b4a580e67f404
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-06 15:07:49 +0530

    Add files via upload

---
+++ def_cr_first_class_phone.py
@@ -0,0 +1,16 @@
+# creating the first class
+#creating a class phone
+class phone:
+    def make_call(self):
+        print("making phone call")
+    def play_game(self):
+        print("playing game")
+pi = phone()
+pi.make_call()
+pi.play_game()
+
+
+#class - key word
+# self - reference call
+# make_call & play_game - method
+#pi - object
